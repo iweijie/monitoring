5474091b23fa44e69df00b8867d5ab0ba1a5a07c
iweijie <375030086@qq.com>
2022-12-19 14:39:54 +0800

    fix: 修改bable

--- src/index.ts
+++ src/index.ts
@@ -1,34 +1,47 @@
-import { Monitor } from "./lib/monitor";
+import ErrorStackParser from "error-stack-parser";
+
+import { jsonStringifySafe as stringify } from "./lib/util";
 
 import { observer } from "./lib/observer";
 
-export * from "./lib/monitor";
-export * from "./lib/ajaxInterceptor";
-export * from "./lib/fetchInterceptor";
-export * from "./lib/errorObserver";
-export * from "./lib/performance";
-export * from "./types/index";
+import { Monitor } from "./lib/monitor";
 
 export default Monitor;
 
-const monitor = Monitor.init({
-  report: {
-    watch: true,
-  },
-  http: {
-    fetch: true,
-    ajax: true,
-    ignoreRules: [],
-  },
-  performance: true,
-  isSpa: true,
-});
+/** 防止 monitor 错误导致主程序挂掉 */
+try {
+  const monitor = Monitor.init({
+    report: {
+      watch: true,
+    },
+    http: {
+      fetch: true,
+      ajax: true,
+      ignoreRules: [],
+    },
+    performance: true,
+    isSpa: true,
+  });
 
-if (window) {
-  window.DJObserver = observer;
-  window.DJMonitor = Monitor;
+  if (window) {
+    window.DJObserver = observer;
+    window.DJMonitor = Monitor;
+  }
+
+  monitor.on("event", (...res) => {
+    observer.preEmit("monitor", res);
+  });
+} catch (error) {
+  const stackTrace =
+    error instanceof Error ? ErrorStackParser.parse(error) : [];
+  const errorObj = {
+    msg: "monitor加载失败",
+    url: "",
+    line: "",
+    column: "",
+    stackTrace: stringify(stackTrace),
+    errorType: "monitor-load-error",
+  };
+
+  observer.preEmit("monitor", errorObj);
 }
-
-monitor.on("event", (...res) => {
-  observer.preEmit("monitor", res);
-});
